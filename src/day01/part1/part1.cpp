ba2eb0c09f58ba4710d5b765cf6040396669d5db
Austin Bachurski <austinbachurski@gmail.com>
2026-02-27 12:51:17 -0700

    Day 1 part 1 solution.

--- src/day01/part1/part1.cpp
+++ src/day01/part1/part1.cpp
@@ -1,19 +1,78 @@
 #include "day01/part1/part1.hpp"
 #include "common/common.hpp"
 
-#include <charconv>
-#include <fstream>
-#include <print>
+#include <ranges>
 #include <string>
 #include <string_view>
 
+
+namespace
+{
+
+int rotateLeft(int dial, int &counter, int const ticks)
+{
+    dial -= ticks;
+
+    while (dial < 0)
+    {
+        dial += 100;
+    }
+
+    if (dial == 0) { ++counter; }
+
+    return dial;
+}
+
+int rotateRight(int dial, int &counter, int const ticks)
+{
+    dial += ticks;
+
+    while (dial > 99)
+    {
+        dial -= 100;
+    }
+
+    if (dial == 0) { ++counter; }
+
+    return dial;
+}
+
+}
 
 namespace day01::part1
 {
 
 std::string solve()
 {
-    return "Part 1 solution.";
+    std::string file{ common::readEntireInputFile("day01.txt") };
+
+    auto rotations{ file | std::views::split('\n')
+                         | std::views::transform([](auto &&each)
+                             { return std::string_view{ each }; }) };
+
+    int counter{};
+    int dial{ 50 };
+
+    for (auto each : rotations)
+    {
+        if (each.empty()) { break; }
+
+        if (each.at(0) == 'L')
+        {
+            dial = rotateLeft(dial,
+                              counter,
+                              common::stringviewToNumber<int>(each.substr(1)));
+        }
+        else
+        {
+            dial = rotateRight(dial,
+                               counter,
+                               common::stringviewToNumber<int>(each.substr(1)));
+        }
+    }
+
+
+    return std::to_string(counter);
 }
 
 } // namespace day01::part1
