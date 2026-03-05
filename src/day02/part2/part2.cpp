c45bbbcb77c99921e79f46b35ee8597e00d2dda4
Austin Bachurski <austinbachurski@gmail.com>
2026-03-05 12:06:12 -0700

    Day 2 part 2.

--- src/day02/part2/part2.cpp
+++ src/day02/part2/part2.cpp
@@ -1,19 +1,89 @@
-#include "part2.hpp"
+#include "day02/part2/part2.hpp"
+#include "common/common.hpp"
 
-#include <charconv>
-#include <fstream>
-#include <print>
+#include <algorithm>
 #include <string>
 #include <string_view>
 
+
+namespace
+{
+
+bool isInvalid(std::size_t number)
+{
+    auto valueStr{ std::to_string(number) };
+
+    std::string_view value{ valueStr };
+
+    std::string_view portion;
+
+    for (auto width : std::views::iota(1uz, value.size()))
+    {
+        portion = value.substr(0, width);
+
+        auto chunks{ value | std::views::chunk(width) 
+                           | std::views::transform([](auto &&chunk) {
+                                return std::string_view{ chunk };
+            }) };
+
+        if (std::ranges::all_of(chunks, [portion] (auto chunk) {
+            return portion == chunk; }))
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+std::size_t checkRange(std::string_view pair)
+{
+    auto values{ common::splitStringOn(pair, '-') };
+
+    auto first{ *values.begin() };
+    auto second{ *(++values.begin()) };
+
+    auto begin{ common::stringviewToNumber<std::size_t>(first) };
+    auto end{ common::stringviewToNumber<std::size_t>(second) };
+
+    std::size_t invalids{};
+
+    for (auto value : std::views::iota(begin, end + 1))
+    {
+        if (value < 11) { continue; }
+
+        if (isInvalid(value))
+        {
+            invalids += value;
+        }
+    }
+
+    return invalids;
+}
+
+} // anonymous namespace
 
 namespace day02::part2
 {
 
 std::string solve()
 {
-    return "Part 2 solution.";
+    std::string file{ common::readEntireInputFile("day02.txt") };
+
+    auto pairs{ common::splitStringOn(file, ',') };
+
+    std::size_t sum{};
+
+    for (auto pair : pairs)
+    {
+        if (pair.empty()) { break; }
+
+        sum += checkRange(pair);
+    }
+
+    return std::to_string(sum);
 }
 
 } // namespace day02::part2
+
 
