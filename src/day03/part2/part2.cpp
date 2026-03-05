8124be4c4ca8961408a8d023defbb5e48739a68a
Austin Bachurski <austinbachurski@gmail.com>
2026-03-05 15:28:34 -0700

    Day 3 part 2.

--- src/day03/part2/part2.cpp
+++ src/day03/part2/part2.cpp
@@ -1,18 +1,69 @@
 #include "part2.hpp"
+#include "common/common.hpp"
 
-#include <charconv>
-#include <fstream>
-#include <print>
+#include <cstddef>
+#include <ranges>
 #include <string>
 #include <string_view>
 
+
+namespace
+{
+
+std::size_t maxFromBank([[maybe_unused]] std::string_view bank)
+{
+    std::string value;
+    value.reserve(12);
+
+    char valueToFind{ '9' };
+    std::size_t previousChars{};
+
+    while (valueToFind != '0')
+    {
+        for (auto const [i, c] : std::views::enumerate(bank)
+        | std::views::take(bank.size() - 11uz + value.size())
+        | std::views::drop(previousChars))
+        {
+            if (c == valueToFind)
+            {
+                value += c;
+
+                if (value.size() == 12uz)
+                {
+                    return common::stringviewToNumber<std::size_t>(value);
+                }
+
+                previousChars = static_cast<std::size_t>(i) + 1u;
+                valueToFind = ':';
+                break;
+            }
+        }
+
+        --valueToFind;
+    }
+
+    return 0;
+}
+
+} // anonymous namespace
 
 namespace day03::part2
 {
 
 std::string solve()
 {
-    return "Part 2 solution.";
+    std::string file{ common::readEntireInputFile("day03.txt") };
+
+    auto banks{ common::splitStringOn(file, '\n') };
+
+    std::size_t sum{};
+
+    for (auto bank : banks)
+    {
+        sum += maxFromBank(bank);
+    }
+
+    return std::to_string(sum);
 }
 
 } // namespace day03::part2
