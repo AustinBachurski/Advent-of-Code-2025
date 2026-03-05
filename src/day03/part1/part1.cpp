fa259bb518e8fcab2fa26738f1018337a8597038
Austin Bachurski <austinbachurski@gmail.com>
2026-03-05 15:28:27 -0700

    Day 3 part 1.

--- src/day03/part1/part1.cpp
+++ src/day03/part1/part1.cpp
@@ -1,11 +1,31 @@
 #include "day03/part1/part1.hpp"
 #include "common/common.hpp"
 
-#include <charconv>
-#include <fstream>
-#include <print>
+#include <algorithm>
 #include <string>
 #include <string_view>
+
+
+namespace
+{
+
+int maxTwoFromBank(std::string_view bank)
+{
+    if (bank.empty()) { return 0; }
+
+    auto maxIter{ std::ranges::max_element(bank) };
+    int max{ *maxIter - 48 };
+
+    auto before{ std::max_element(bank.begin(), maxIter) };
+    int valueBefore{ before != maxIter ? (*before - 48) * 10 + max : max };
+
+    auto after{ std::max_element(++maxIter, bank.end()) };
+    int valueAfter{ after != bank.end() ? max * 10 + (*after - 48) : max };
+
+    return valueBefore > valueAfter ? valueBefore : valueAfter;
+}
+
+} // anonymous namespace
 
 
 namespace day03::part1
@@ -13,7 +33,18 @@ namespace day03::part1
 
 std::string solve()
 {
-    return "Part 1 solution.";
+    std::string file{ common::readEntireInputFile("day03.txt") };
+
+    auto banks{ common::splitStringOn(file, '\n') };
+
+    int sum{};
+
+    for (auto bank : banks)
+    {
+        sum += maxTwoFromBank(bank);
+    }
+
+    return std::to_string(sum);
 }
 
 } // namespace day03::part1
