031aeea5c186a099b85475566b029e04e0f69d22
Austin Bachurski <austinbachurski@gmail.com>
2026-03-06 17:10:47 -0700

    Day 5 part 1.

--- src/day05/part1/part1.cpp
+++ src/day05/part1/part1.cpp
@@ -1,19 +1,84 @@
 #include "day05/part1/part1.hpp"
 #include "common/common.hpp"
 
-#include <charconv>
-#include <fstream>
-#include <print>
+#include <algorithm>
+#include <cstddef>
+#include <span>
 #include <string>
 #include <string_view>
+#include <vector>
 
 
 namespace day05::part1
 {
 
+struct Values
+{
+    std::size_t min{};
+    std::size_t max{};
+};
+
+std::vector<Values> getFreshValues(auto freshRanges)
+{
+    std::vector<Values> freshValues;
+
+    std::size_t begin{};
+    std::size_t end{};
+
+    for (auto const range : freshRanges)
+    {
+        if (range.empty())
+        { continue; }
+
+        auto values{ common::splitStringOn(range, '-') };
+        
+        begin = common::stringviewToNumber<std::size_t>(*values.begin());
+        end = common::stringviewToNumber<std::size_t>(*++values.begin());
+
+        freshValues.emplace_back(begin, end);
+    }
+
+    return freshValues;
+}
+
+std::size_t countFreshIngredients(
+    std::span<Values const> freshValues, auto ingredients)
+{
+    std::size_t count{};
+
+    std::size_t value{};
+
+    for (auto const id : ingredients)
+    {
+        if (id.empty())
+        { continue; }
+
+        value = common::stringviewToNumber<std::size_t>(id);
+
+        if (std::ranges::any_of(freshValues, [value](Values pair)
+                        { return value >= pair.min && value <= pair.max; } ))
+        {
+            ++count;
+        }
+    }
+
+    return count;
+}
+
 std::string solve()
 {
-    return "Part 1 solution.";
+    std::string file{ common::readEntireInputFile("day05.txt") };
+
+    using namespace std::string_view_literals;
+    auto separated{ common::splitStringOn(file, "\n\n"sv) };
+
+    auto freshRanges{ common::splitStringOn(*separated.begin(), '\n') };
+    auto ingredients{ common::splitStringOn(*++separated.begin(), '\n') };
+
+    auto freshValues = getFreshValues(freshRanges);
+    
+
+    return std::to_string(countFreshIngredients(freshValues, ingredients));
 }
 
 } // namespace day05::part1
