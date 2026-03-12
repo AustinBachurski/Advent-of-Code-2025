21be7701d3170612078482f93983e29be1543baf
Austin Bachurski <austinbachurski@gmail.com>
2026-03-12 14:07:25 -0600

    Day 7 part 2.

--- src/day07/part2/part2.cpp
+++ src/day07/part2/part2.cpp
@@ -1,30 +1,63 @@
 #include "day07/part2/part2.hpp"
 #include "common/common.hpp"
 
+#include <compare>
 #include <cstddef>
+#include <functional>
 #include <ranges>
 #include <string>
 #include <string_view>
+#include <unordered_map>
 
 
 namespace
 {
 
-std::size_t findPath(std::size_t const beam, auto diagram, std::ptrdiff_t atRow)
+struct Beam
 {
+    std::size_t index{};
+    std::ptrdiff_t row{};
+
+    auto operator<=>(Beam const &other) const = default;
+};
+
+struct BeamHasher
+{
+    std::size_t operator()(Beam const beam) const noexcept
+    {
+        auto h1{ std::hash<std::size_t>{}(beam.index) };
+        auto h2{ std::hash<std::ptrdiff_t>{}(beam.row) };
+        return h1 ^ (h2 << 1);
+    }
+};
+
+using BeamCache = std::unordered_map<Beam, std::size_t, BeamHasher>;
+std::size_t findPath(Beam const beam, auto diagram, BeamCache &cache)
+{
+    if (cache.contains(beam))
+    {
+        return cache.at(beam);
+    }
+
     std::size_t paths{};
 
     for (auto [row, line] : std::views::enumerate(diagram)
-                            | std::views::drop(atRow))
+                            | std::views::drop(beam.row + 1))
     {
-        if (line.at(beam) == '^')
+        if (line.at(beam.index) == '^')
         {
-            paths += findPath(beam - 1, diagram, row);
-            paths += findPath(beam + 1, diagram, row);
+            Beam const left { .index = beam.index - 1, .row = row };
+            Beam const right{ .index = beam.index + 1, .row = row };
+
+            paths += findPath(left, diagram, cache);
+            paths += findPath(right, diagram, cache);
+
+            cache.insert_or_assign(beam, paths);
             return paths;
         }
     }
 
+    cache.insert_or_assign(beam, 1uz);
     return 1uz;
 }
 
@@ -37,9 +70,12 @@ std::string solve()
 {
     std::string file{ common::readEntireInputFile("day07.txt") };
     auto diagram{ common::splitStringOn(file, '\n') };
-    std::size_t beam{ (*diagram.begin()).find('S') };
+    Beam primaryBeam{ (*diagram.begin()).find('S'), 0 };
 
-    std::size_t totalPaths{ findPath(beam, diagram, 1) };
+    std::unordered_map<Beam, std::size_t, BeamHasher> cache;
+    cache.reserve(1024);
+
+    std::size_t totalPaths{ findPath(primaryBeam, diagram, cache) };
 
     return std::to_string(totalPaths);
 }
