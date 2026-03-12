7fdb87d466bb31038fa7b71bfdfc892c90f24e5a
Austin Bachurski <austinbachurski@gmail.com>
2026-03-12 09:10:55 -0600

    Day 7 part 1.

--- src/day07/part1/part1.cpp
+++ src/day07/part1/part1.cpp
@@ -1,7 +1,9 @@
 #include "day07/part1/part1.hpp"
 #include "common/common.hpp"
 
-#include <print>
+#include <cstddef>
+#include <ranges>
+#include <set>
 #include <string>
 #include <string_view>
 #include <vector>
@@ -15,14 +17,42 @@ std::string solve()
     std::string file{ common::readEntireInputFile("day07.txt") };
     auto diagram{ common::splitStringOn(file, '\n') };
 
-    std::vector<std::size_t> beams;
+    std::set<std::size_t> beams;
+    beams.insert((*diagram.begin()).find('S'));
 
+    std::size_t splits{};
+    std::vector<std::size_t> toInsert;
+    std::vector<std::size_t> toRemove;
 
+    for (auto line : diagram | std::views::drop(1))
+    {
+        for (auto const i : beams)
+        {
+            if (line.at(i) == '^')
+            {
+                toInsert.push_back(i - 1);
+                toInsert.push_back(i + 1);
+                toRemove.push_back(i);
+                ++splits;
+            }
+        }
 
+        for (auto const rem : toRemove)
+        {
+            beams.erase(rem);
+        }
 
+        toRemove.clear();
 
+        for (auto const ins : toInsert)
+        {
+            beams.insert(ins);
+        }
 
-    return "Part 1 solution.";
+        toInsert.clear();
+    }
+
+    return std::to_string(splits);
 }
 
 } // namespace day07::part1
