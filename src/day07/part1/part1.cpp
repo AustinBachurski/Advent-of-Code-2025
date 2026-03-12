4f9e281f500b7b4a43f6dcf191c1d1d16b992213
Austin Bachurski <austinbachurski@gmail.com>
2026-03-11 18:26:20 -0600

    Start day 7.

--- src/day07/part1/part1.cpp
+++ src/day07/part1/part1.cpp
@@ -1,11 +1,10 @@
 #include "day07/part1/part1.hpp"
 #include "common/common.hpp"
 
-#include <charconv>
-#include <fstream>
 #include <print>
 #include <string>
 #include <string_view>
+#include <vector>
 
 
 namespace day07::part1
@@ -13,6 +12,16 @@ namespace day07::part1
 
 std::string solve()
 {
+    std::string file{ common::readEntireInputFile("day07.txt") };
+    auto diagram{ common::splitStringOn(file, '\n') };
+
+    std::vector<std::size_t> beams;
+
+
+
+
+
+
     return "Part 1 solution.";
 }
 
