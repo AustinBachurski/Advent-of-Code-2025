4e139745634e215b947804cf78697002561f213e
Austin Bachurski <austinbachurski@gmail.com>
2026-03-13 16:02:44 -0600

    Start of part 2.

--- src/day09/part2/part2.cpp
+++ src/day09/part2/part2.cpp
@@ -1,18 +1,80 @@
-#include "part2.hpp"
+#include "day09/part2/part2.hpp"
+#include "common/common.hpp"
 
-#include <charconv>
-#include <fstream>
+#include <algorithm>
+#include <cassert>
+#include <compare>
+#include <cstdint>
+#include <cstdlib>
 #include <print>
 #include <string>
 #include <string_view>
+#include <vector>
 
+
+namespace
+{
+
+struct Point
+{
+    int64_t x{};
+    int64_t y{};
+
+    auto operator<=>(Point const &other) const = default;
+};
+
+std::vector<Point> extractPointsFromInput(auto coordinates)
+{
+    std::vector<Point> points;
+
+    for (auto coordinate : coordinates)
+    {
+        auto pair{ common::splitStringOn(coordinate, ',') };
+        assert(std::distance(pair.begin(), pair.end()) == 2);
+        auto x{ common::stringviewToNumber<int64_t>(*pair.begin()) };
+        auto y{ common::stringviewToNumber<int64_t>(*++pair.begin()) };
+        points.emplace_back(x, y);
+    }
+
+    return points;
+}
+
+int64_t calculateMaxArea(std::span<Point const> points)
+{
+    int64_t max{};
+
+    for (auto const p : points)
+    {
+        for (auto const otherP : points)
+        {
+            if (p == otherP)
+            { continue; }
+
+            // Add 1 for inclusive area.
+            int64_t xResult{ std::abs(p.x - otherP.x) + 1 };
+            int64_t yResult{ std::abs(p.y - otherP.y) + 1 };
+
+            max = std::max(max, xResult * yResult);
+
+             std::println("{}:{},{}|{},{}", xResult * yResult, p.x, p.y, otherP.x, otherP.y);
+        }
+    }
+
+    return max;
+}
+
+} // anonymous namespace
 
 namespace day09::part2
 {
 
 std::string solve()
 {
-    return "Part 2 solution.";
+    auto file{ common::readEntireInputFile("day09.txt") };
+    auto coordinates{ common::splitStringOn(file, '\n') };
+    auto points{ extractPointsFromInput(coordinates) };
+
+    return std::to_string(calculateMaxArea(points));
 }
 
 } // namespace day09::part2
